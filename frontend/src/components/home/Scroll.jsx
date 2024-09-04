7959f52977661e91cde2a940e0f4571e0135d093
hana <gksk3185@gmail.com>
2024-09-04 20:48:34 +0900

    feat: FE code add 2

--- frontend/src/components/home/Scroll.jsx
+++ frontend/src/components/home/Scroll.jsx
@@ -3,6 +3,74 @@ import React from "react";
 export const Scroll = () => {
     
     return (
-        <></>
+        <>
+            <div>
+                <img src="" alt="logo" />
+            </div>
+            <br />
+            
+            <div>
+                <p>트래블 레이트와 함께라면</p>
+                <p>여행이 현명해집니다.</p>
+            </div>
+            <br />
+
+            <div>
+                <img src="" alt="airplane" />
+            </div>
+            <br />
+
+            <div>
+                <div>
+                    <p>똑똑한 소비자를 위한</p>
+                    <p>가성비 여행지 추천</p>
+                </div>
+                <br />
+
+                <div>
+                    <img src="" alt="currency" />
+                    <p>지원통화</p>
+                    <p>23개</p>
+                </div>
+                <br />
+
+                <div>
+                    <img src="" alt="card" />
+                    <p>카드추천</p>
+                    <p>평균 30종 이상</p>
+                </div> 
+                <br />
+
+                <div>
+                    <img src="" alt="exchange" />
+                    <p>원하는 환율에 환전이 가능하도록,</p>
+                    <p>알림 서비스</p>
+                </div>
+                <br />
+            </div>
+
+
+            <div>
+                <img src="" alt="destination" />
+                <p>지금 현재 가기 좋은 여행지 추천받고,</p>
+                <p>빠르게 여행계획 세우자!</p>
+            </div>
+            <br />
+
+            <div>
+                <img src="" alt="card-recommendation" />
+                <p>여행지 환율 우대받을 수 있는 카드 추천받고,</p>
+                <p>몰랐던 혜택 누리자!</p>
+            </div>
+            <br />
+
+            <div>
+                <img src="" alt="alert" />
+                <p>알림받고 내가 정한 환율대로</p>
+                <p>현명한 여행계획 세우자!</p>
+            </div>
+            <br />
+
+        </>
     )
 }
